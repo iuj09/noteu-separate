ce30b7734ec14fc6405081f0fc9d9b4878bd0802
IUJin <iuj09@naver.com>
2023-12-20 16:23:32 +0900

    #12 feat : 자료실 상세 페이지 파일 다운로드 / 수정 / 삭제
    - 자료실 상세 페이지 파일 다운로드 기능 추가
    - edit 클릭 시에는 Update 페이지로 이동하도록 함
    - delete 클릭 시에는 해당 게시글 삭제 기능 구현

--- front/src/pages/apps/ReferenceRoom/Detail/Files.jsx
+++ front/src/pages/apps/ReferenceRoom/Detail/Files.jsx
@@ -1,34 +1,52 @@
-import { Card, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Card, Row, Button } from 'react-bootstrap';
 
 const Files = (props) => {
 
 	const { reference } = props;
 
+	const token = localStorage.getItem("_NOTEU_AUTH").replace(/^"(.*)"$/, '$1');
+
+	const download = (id, referenceName) => {
+		axios.get(`http://localhost:8081/subjects/1/references/down?id=${id}&referenceName=${referenceName}`, {headers:{Authorization:token}, responseType:'blob'}, )
+		.then(res => {
+			if(res.status === 200) {
+				const url = window.URL.createObjectURL(new Blob([res.data]));
+				const link = document.createElement('a');
+				link.href = url;
+				link.setAttribute('download', referenceName);
+				document.body.appendChild(link);
+				link.click();
+			} else {
+				console.log("error : " + res.status);
+			}
+		})
+	}
+
 	return (
 		<Card>
 			<Card.Body>
-				<h5 className="card-title mb-3">DownLoad Files</h5>
+				<h5 className="card-title mb-3">DownLoadFiles</h5>
 
 				{reference?.map((file) => (
-					<Card className="mb-1 shadow-none border">
+					<Card key={file.id} className="mb-1 shadow-none border">
 						<div className="p-2">
 							<Row className="align-items-center">
 								<div className="col-auto">
 									<div className="avatar-sm">
-										<span className="avatar-title rounded">{file.referenceType}</span>
+										<span className="avatar-title rounded">{file.referenceType.split('/')[0]}</span>
 									</div>
 								</div>
 								<div className="col ps-0">
-									<Link to="" className="text-muted fw-bold">
+									<div className="text-muted fw-bold">
 										{file.referenceName}
-									</Link>
+									</div>
 									<p className="mb-0">{file.referenceSize} MB</p>
 								</div>
 								<div className="col-auto">
-									<Link to="" className="btn btn-link btn-lg text-muted">
+									<Button variant="link" className="btn btn-link btn-lg text-muted" onClick={() => download(file.id, file.referenceName)}>
 										<i className="ri-download-2-line"></i>
-									</Link>
+									</Button>
 								</div>
 							</Row>
 						</div>
